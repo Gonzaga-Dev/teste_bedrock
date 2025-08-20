0a77d91890d10b8d5304d15d323ddb78cc9eb001
Ricardo Gonzaga <123505650+Gonzaga-Dev@users.noreply.github.com>
2025-08-20 18:08:42 -0300

    Update AuthGate.tsx

--- app/AuthGate.tsx
+++ app/AuthGate.tsx
@@ -1,12 +1,20 @@
 "use client";
-
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";   // ← caminho relativo a partir de /app
+import outputs from "../amplify_outputs.json";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
 export default function AuthGate({ children }: { children: React.ReactNode }) {
-  return <Authenticator>{() => <>{children}</>}</Authenticator>;
+  return (
+    <Authenticator
+      loginMechanisms={["email"]}
+      hideSignUp
+      // opcional: garantir que sempre abra no Sign In
+      initialState="signIn"
+    >
+      {() => <>{children}</>}
+    </Authenticator>
+  );
 }
